d267998c6eb3717047614bd3ee5b63405f3c2313
Rangel Henrique Fialho de Moura <65991045+rangelhenriquef@users.noreply.github.com>
2022-02-19 18:55:24 -0300

    Create valida_cpf_cnpj.py

---
+++ valida_cpf_cnpj.py
@@ -0,0 +1,31 @@
+from validate_docbr import CPF, CNPJ
+
+
+class OldValidaCPFouCNPJ:
+    def __init__(self, cpf_cnpj):
+        self.documento = str(cpf_cnpj)
+        if self.valida_doc(self.documento):
+            self.cpf_cnpj = self.documento
+        else:
+            raise ValueError(f'Documento inválido')
+
+    def valida_doc(self, documento) -> bool:
+        self.documento = documento
+        if len(self.documento) == 11:
+            return self.valida_cpf()
+        elif len(self.documento) == 14:
+            return self.valida_cnpj()
+        else:
+            raise ValueError(f'Quantidade de dígitos inválida!')
+
+    def valida_cpf(self) -> bool:
+        validador = CPF()
+        return validador.validate(self.documento)
+
+    def valida_cnpj(self) -> bool:
+        validador = CNPJ()
+        return validador.validate(self.documento)
+
+    def __str__(self):
+        documento = CPF() if len(self.documento) == 11 else CNPJ()
+        return documento.mask(self.documento) + ' é válido!'
